f4267f9d8147b914fea131d3086608cb79bc22b0
Naloween <40539093+Naloween@users.noreply.github.com>
2022-09-23 08:34:35 +0200

    feat: tests level5

--- tests/test_island1/test_level5.py
+++ tests/test_island1/test_level5.py
@@ -1,8 +1,9 @@
 
-from poetry_tutorial.island1.level1 import mult_two
+from poetry_tutorial.island1.level5 import most_frequent
 
-def test_level1_1():
-    assert mult_two(3, 2) == 6
-    
-def test_level1_2():
-    assert mult_two(0, 1) == 0
+def test_level5_1():
+    assert most_frequent(["a", "b", "c", "a", "b", "a"]) == "a"
+
+def test_level5_2():
+    assert most_frequent(["a", "a", "bi", "bi", "bi"]) == "bi"
+
