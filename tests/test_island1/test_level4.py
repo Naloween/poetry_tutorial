adae008d6b90d9eb69a064b60be9275e4da81a0e
Naloween <40539093+Naloween@users.noreply.github.com>
2022-09-23 08:34:26 +0200

    feat: tests level4

--- tests/test_island1/test_level4.py
+++ tests/test_island1/test_level4.py
@@ -1,8 +1,16 @@
 
-from poetry_tutorial.island1.level1 import mult_two
+from poetry_tutorial.island1.level4 import number_length
 
-def test_level1_1():
-    assert mult_two(3, 2) == 6
-    
-def test_level1_2():
-    assert mult_two(0, 1) == 0
+def test_level4_1():
+    assert number_length(10) == 2
+
+def test_level4_2():
+    assert number_length(0) == 1
+
+def test_level4_3():
+    assert number_length(4) == 1
+
+def test_level4_4():
+    assert number_length(44) == 2
+
+
