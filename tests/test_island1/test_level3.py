311f397a33fa154c72b26d41e9a4826dedb87aaf
Naloween <40539093+Naloween@users.noreply.github.com>
2022-09-23 08:34:18 +0200

    feat: tests level 3

--- tests/test_island1/test_level3.py
+++ tests/test_island1/test_level3.py
@@ -1,8 +1,12 @@
 
-from poetry_tutorial.island1.level1 import mult_two
+from poetry_tutorial.island1.level3 import is_acceptable_password
 
-def test_level1_1():
-    assert mult_two(3, 2) == 6
-    
-def test_level1_2():
-    assert mult_two(0, 1) == 0
+def test_level3_1():
+    assert is_acceptable_password("short") == False
+
+def test_level3_2():
+    assert is_acceptable_password("muchlonger") == True
+
+def test_level3_3():
+    assert is_acceptable_password("ashort") == False
+
